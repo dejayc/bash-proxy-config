8c8cd50e17d61fb6dee07a09ec1339ea6e7b918b
Dejay Clayton <dejay@dejayclayton.com>
2018-08-10 19:50:57 -0400

    Added test proxy configurations

--- config.sh
+++ config.sh
@@ -2,4 +2,13 @@
 
 # Proxy default
 PROXY_DEFAULT_URL='http://proxy.example.com/'
+PROXY_DEFAULT_FTP_URL='ftp://proxy.example.com/'
+PROXY_DEFAULT_HTTP_URL=''
+PROXY_DEFAULT_HTTPS_URL=''
 PROXY_DEFAULT_NO_PROXY='localhost,127.0.0.0/8,[::]'
+
+# Proxy configuration named 'local'
+PROXY_LOCAL_URL='http://localhost:8080/'
+
+# Proxy configuration named '8081'
+PROXY_8081_URL='http://localhost:80801/'

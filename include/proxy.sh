62413d75320bf586f60abf523691dc32feb9ff9d
Dejay Clayton <dejay@dejayclayton.com>
2018-08-10 19:57:03 -0400

    Stubbed out initial function hierarchy

--- include/proxy.sh
+++ include/proxy.sh
@@ -9,3 +9,37 @@
 # disable them during actual use, so that this script doesn't adversely
 # interfere with the environment or specified commands.
 set -eu
+
+function proxy-call()
+{
+  proxy-exec "${@:+${@}}"
+}
+ 
+function proxy-exec()
+{
+  :
+}
+
+function proxy-cat()
+{
+  IFS='' read -r -d '' OUTPUT ||:
+  echo "${OUTPUT}"
+}
+
+function proxy-get-settings()
+{
+  :
+}
+
+function proxy-show()
+{
+  printf '%s: [%s]\n' \
+    'FTP_PROXY' "${FTP_PROXY-}" \
+    'HTTP_PROXY' "${HTTP_PROXY-}" \
+    'HTTPS_PROXY' "${HTTPS_PROXY-}" \
+    'ftp_proxy' "${ftp_proxy-}" \
+    'http_proxy' "${http_proxy-}" \
+    'https_proxy' "${https_proxy-}" \
+    'no_proxy' "${no_proxy-}" \
+    'NODE_TLS_REJECT_UNAUTHORIZED' "${NODE_TLS_REJECT_UNAUTHORIZED-}"
+}

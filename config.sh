1c8cffc766bfa07af89cd951c61649e940e887af
Dejay Clayton <dejay@dejayclayton.com>
2018-08-10 22:39:26 -0400

    Added crazy cool configuration default logic

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+PROXY_DEFAULT_TO='default'
 
-# Proxy default
+# Proxy default fallbacks
 PROXY_DEFAULT_FOR='nonlocal'
 PROXY_DEFAULT_URL='http://proxy.example.com/'
 PROXY_DEFAULT_FTP_URL='ftp://proxy.example.com/'
@@ -10,8 +11,10 @@ PROXY_DEFAULT_NO_PROXY='localhost,127.0.0.0/8,[::]'
 
 # Proxy configuration named 'local'
 PROXY_LOCAL_FOR='nonlocal'
+PROXY_LOCAL_DEFAULT=''
 PROXY_LOCAL_URL='http://localhost:8080/'
 
 # Proxy configuration named '8081'
 PROXY_8081_FOR='all'
+PROXY_8081_DEFAULT='local'
 PROXY_8081_URL='http://localhost:80801/'
--- include/proxy.sh
+++ include/proxy.sh
@@ -202,7 +202,7 @@ ERROR: Unable to retrieve a list of proxy configuration variables.  Please
   ensure that:
   1. one or more variables exist with prefix 'PROXY_'.  For example,
      to configure a proxy named 'local', define variables such as
-     'PROXY_LOCAL_HTTP_URL', 'PROXY_LOCAL_URL', etc.
+     'PROXY_LOCAL_HTTP_URL', 'PROXY_LOCAL_DEFAULT', etc.
   2. bash command 'compgen -A variable PROXY_' works
 SETTINGS FILE: '${SETTINGS_FILE}'
 :ERR
@@ -215,8 +215,6 @@ SETTINGS FILE: '${SETTINGS_FILE}'
   declare -a PROXIES=()
   local PROXIES_LOOKUP=''
 
-  declare -a SETTINGS=()
-
   declare -i I=0
   while [ ${I} -lt ${#PROXY_VARS[@]} ]
   do
@@ -224,15 +222,12 @@ SETTINGS FILE: '${SETTINGS_FILE}'
     let I+=1
 
     [[ "${PROXY_VAR}" =~ ^PROXY_([0-9A-Z]+)_\
-(((FTP_|HTTP_|HTTPS_)?URL)|NO_PROXY)$ ]] && {
+(DEFAULT|((FTP_|HTTP_|HTTPS_)?URL)|NO_PROXY)$ ]] && {
       local PROXY="${BASH_REMATCH[1]}"
       [[ "${PROXIES_LOOKUP}" =~ \[${PROXY}\] ]] || {
         PROXIES[${#PROXIES[@]}]="${PROXY}"
         PROXIES_LOOKUP="[${PROXY}]${PROXIES_LOOKUP}"
       }
-      local ASSIGNMENT=''
-      printf -v ASSIGNMENT '%s=%q' "${PROXY_VAR}" "${!PROXY_VAR-}"
-      SETTINGS[${#SETTINGS[@]}]="${ASSIGNMENT}"
     }
   done
 
@@ -241,11 +236,225 @@ SETTINGS FILE: '${SETTINGS_FILE}'
 ERROR: Unable to retrieve a list of proxy configuration variables.  Please
   ensure that one or more variables exist with prefix 'PROXY_'.  For example,
   to configure a proxy named 'local', define variables such as
-  'PROXY_LOCAL_HTTP_URL', 'PROXY_LOCAL_URL', etc.
+  'PROXY_LOCAL_HTTP_URL', 'PROXY_LOCAL_DEFAULT', etc.
 SETTINGS FILE: '${SETTINGS_FILE}'
 :ERR
     return 2
   } >&2
+
+  local ORDERED=''
+
+  declare -i I=0
+  while [ ${I} -lt ${#PROXIES[@]} ]
+  do
+    local PROXY="${PROXIES[I]}"
+    let I+=1
+
+    local DEFAULT_VAR="PROXY_${PROXY}_DEFAULT"
+    local DEFAULT="$( proxy-ucase "${!DEFAULT_VAR-}" )"
+
+    [[ -n "${DEFAULT}" ]] || {
+      [[ "${ORDERED}" =~ \[${PROXY}\] ]] || ORDERED="[${PROXY}]${ORDERED}"
+      continue
+    }
+
+    local PROXY_LC="$( proxy-lcase "${PROXY}" )"
+    local DEFAULT_LC="$( proxy-lcase "${DEFAULT}" )"
+
+    [[ "${DEFAULT}" != "${PROXY}" ]] || {
+      proxy-cat <<:ERR
+ERROR: Proxy configuration cannot default to itself
+PROXY: '${PROXY_LC}'
+:ERR
+      return 2
+    } >&2
+
+    [[ "${DEFAULT}" =~ ^[0-9A-Z]+$ ]] || {
+      proxy-cat <<:ERR
+ERROR: Proxy configuration contains invalid default configuration name
+PROXY: '${PROXY_LC}'
+DEFAULT: '${DEFAULT_LC}'
+:ERR
+      return 2
+    } >&2
+
+    [[ "${PROXIES_LOOKUP}" =~ \[${DEFAULT}\] ]] || {
+      proxy-cat <<:ERR
+ERROR: Proxy configuration references undefined default configuration
+PROXY: '${PROXY_LC}'
+DEFAULT: '${DEFAULT_LC}'
+:ERR
+      return 2
+    } >&2
+
+    if [[ "${ORDERED}" =~ \[${PROXY}\] ]]
+    then
+      if [[ "${ORDERED}" =~ \[${PROXY}\].*\[${DEFAULT}\] ]]
+      then {
+        proxy-cat <<:ERR
+ERROR: Proxy configuration has circular dependency upon default configuration
+PROXY: '${PROXY_LC}'
+DEFAULT: '${DEFAULT_LC}'
+:ERR
+        return 2
+      } >&2
+      else
+        ORDERED="${ORDERED//\[${PROXY}\]/[${DEFAULT}][${PROXY}]}"
+      fi
+    else
+      if [[ "${ORDERED}" =~ \[${DEFAULT}\] ]]
+      then
+        ORDERED="${ORDERED//\[${DEFAULT}\]/[${DEFAULT}][${PROXY}]}"
+      else
+        ORDERED="[${DEFAULT}][${PROXY}]${ORDERED}"
+      fi
+    fi
+  done
+
+  ORDERED="${ORDERED//\]\[/ }"
+  ORDERED="${ORDERED#\[}"
+  ORDERED="${ORDERED%\]}"
+  declare -a ORDERED_PROXIES=()
+  read -a ORDERED_PROXIES -d '' <<< "${ORDERED}"
+
+  declare -a SETTINGS=()
+
+  local DEFAULT_TO_VAR="PROXY_DEFAULT_TO"
+  local DEFAULT_TO="$( proxy-ucase "${!DEFAULT_TO_VAR-}" )"
+  local DEFAULT_TO_LC="$( proxy-lcase "${DEFAULT_TO}" )"
+
+  [[ -n "${DEFAULT_TO}" ]] && {
+    [[ "${PROXIES_LOOKUP}" =~ \[${DEFAULT_TO}\] ]] || {
+      proxy-cat <<:ERR
+ERROR: Proxy variable 'DEFAULT_TO' references an undefined configuration
+DEFAULT_TO: '${DEFAULT_TO_LC}'
+:ERR
+      return 2
+    } >&2
+
+    declare "${DEFAULT_TO_VAR}"="${DEFAULT_TO}"
+    local ASSIGNMENT=''
+    printf -v ASSIGNMENT '%s=%q' "${DEFAULT_TO_VAR}" "${DEFAULT_TO}"
+    SETTINGS[${#SETTINGS[@]}]="${ASSIGNMENT}"
+  }
+
+  let I=0
+  while [ ${I} -lt ${#ORDERED_PROXIES[@]} ]
+  do
+    local PROXY="${ORDERED_PROXIES[I]}"
+    let I+=1
+
+    local DEFAULT_VAR="PROXY_${PROXY}_DEFAULT"
+    local DEFAULT="$( proxy-ucase "${!DEFAULT_VAR-}" )"
+
+    local PROXY_FOR_VAR="PROXY_${PROXY}_FOR"
+    local PROXY_FOR="$( proxy-lcase "${!PROXY_FOR_VAR-}" )"
+
+    [[ -n "${PROXY_FOR}" ]] && {
+      [[ "${PROXY_FOR}" =~ ^all|nonlocal$ ]] || {
+        proxy-cat <<:ERR
+ERROR: Proxy variable '${PROXY_FOR_VAR}' contains an invalid value.  Valid
+  values are 'all' or 'nonlocal'
+${PROXY_FOR_VAR}: '${PROXY_FOR}'
+:ERR
+        return 2
+      } >&2
+    }
+
+    [[ -z "${PROXY_FOR}" && -n "${DEFAULT}" ]] && {
+      local DEFAULT_FOR_VAR="PROXY_${DEFAULT}_FOR"
+      PROXY_FOR="$( proxy-lcase "${!DEFAULT_FOR_VAR-}" )"
+    }
+
+    declare "${PROXY_FOR_VAR}"="${PROXY_FOR}"
+    local ASSIGNMENT=''
+    printf -v ASSIGNMENT '%s=%q' "${PROXY_FOR_VAR}" "${PROXY_FOR}"
+    SETTINGS[${#SETTINGS[@]}]="${ASSIGNMENT}"
+
+    local PROXY_NO_PROXY_VAR="PROXY_${PROXY}_NO_PROXY"
+    local PROXY_NO_PROXY="${!PROXY_NO_PROXY_VAR-}"
+
+    [[ -z "${PROXY_NO_PROXY}" && -n "${DEFAULT}" ]] && {
+      local DEFAULT_NO_PROXY_VAR="PROXY_${DEFAULT}_NO_PROXY"
+      PROXY_NO_PROXY="${!DEFAULT_NO_PROXY_VAR-}"
+    }
+
+    declare "${PROXY_NO_PROXY_VAR}"="${PROXY_NO_PROXY}"
+    local ASSIGNMENT=''
+    printf -v ASSIGNMENT '%s=%q' "${PROXY_NO_PROXY_VAR}" "${PROXY_NO_PROXY}"
+    SETTINGS[${#SETTINGS[@]}]="${ASSIGNMENT}"
+
+    declare -a ATTRS=( 'URL' )
+    declare -i A=0
+
+    while [ ${A} -lt ${#ATTRS[@]} ]
+    do
+      local ATTR="${ATTRS[A]}"
+      let A+=1
+
+      local DEFAULT_FALLBACK_ATTR_VAR=''
+      local DEFAULT_FALLBACK_ATTR=''
+
+      [[ -n "${DEFAULT}" ]] && {
+        DEFAULT_FALLBACK_ATTR_VAR="PROXY_${DEFAULT}_${ATTR}"
+        DEFAULT_FALLBACK_ATTR="${!DEFAULT_FALLBACK_ATTR_VAR-}"
+      }
+
+      local PROXY_FALLBACK_ATTR_VAR="PROXY_${PROXY}_${ATTR}"
+      local PROXY_FALLBACK_ATTR="${!PROXY_FALLBACK_ATTR_VAR-}"
+
+      declare -a PROTOCOLS=( 'FTP' 'HTTP' 'HTTPS' )
+      declare -i P=0
+
+      while [ ${P} -lt ${#PROTOCOLS[@]} ]
+      do
+        local PROTOCOL="${PROTOCOLS[P]}"
+        let P+=1
+
+        local PROXY_PROTOCOL_ATTR_VAR="PROXY_${PROXY}_${PROTOCOL}_${ATTR}"
+
+        declare -a VARS=(
+          "${PROXY_PROTOCOL_ATTR_VAR}" "${PROXY_FALLBACK_ATTR_VAR}" )
+
+        [[ -n "${DEFAULT}" ]] && {
+          VARS[${#VARS[@]}]="PROXY_${DEFAULT}_${PROTOCOL}_${ATTR}"
+          VARS[${#VARS[@]}]="PROXY_${DEFAULT}_${ATTR}"
+        }
+
+        local SETTING=''
+
+        declare -i V=0
+        while [ ${V} -lt ${#VARS[@]} ]
+        do
+          local VAR="${VARS[V]}"
+          let V+=1
+
+          [[ -n "${!VAR-}" ]] && {
+            SETTING="${!VAR}"
+            break
+          }
+        done
+
+        declare "${PROXY_PROTOCOL_ATTR_VAR}"="${SETTING}"
+        local ASSIGNMENT=''
+        printf -v ASSIGNMENT '%s=%q' \
+          "${PROXY_PROTOCOL_ATTR_VAR}" "${SETTING}"
+        SETTINGS[${#SETTINGS[@]}]="${ASSIGNMENT}"
+      done
+
+      # Update the current proxy protocol-agnostic attribute default value
+      # from the default proxy protocol-agnostic attribute default value, so
+      # that future defaults upon this proxy configuration are propagated.
+      [[ -n "${PROXY_FALLBACK_ATTR}" ]] || \
+        PROXY_FALLBACK_ATTR="${DEFAULT_FALLBACK_ATTR}"
+
+      declare "${PROXY_FALLBACK_ATTR_VAR}"="${PROXY_FALLBACK_ATTR}"
+      local ASSIGNMENT=''
+      printf -v ASSIGNMENT '%s=%q' \
+        "${PROXY_FALLBACK_ATTR_VAR}" "${PROXY_FALLBACK_ATTR}"
+      SETTINGS[${#SETTINGS[@]}]="${ASSIGNMENT}"
+    done
+  done
 
   local ASSIGNMENT=''
   printf -v ASSIGNMENT '%s ' "${PROXIES[@]}"
